627290a01a688c3ba26b62e67212894fd9a2f03f
adammarples <adammarples@gmail.com>
2016-10-08 23:00:12 +0000

    cosmetic changes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import build_matrix
-import algorithm_x
+import build_matrix as bx
+import algorithm_x as ax
 
 def build_soma():
-    grid = build_matrix.build_from_txt('soma.txt', 7, 3)
-    build_matrix.save('soma.csv', grid)
+    grid = bx.build_from_txt('soma.txt', 7, 3)
+    bx.save('soma.csv', grid)
 
 def build_wiki_example():
     universe = {1, 2, 3, 4, 5, 6, 7}
@@ -14,13 +14,13 @@ def build_wiki_example():
     E = {2, 3, 6, 7}
     F = {2, 7}
     sets = [A, B, C, D, E, F]
-    grid = build_matrix.build_from_sets(universe, sets)
-    build_matrix.save('wiki_example.csv', grid)
+    grid = bx.build_from_sets(universe, sets)
+    bx.save('wiki_example.csv', grid)
 
 def solve(csv_name):
-    grid = algorithm_x.load(csv_name)
+    grid = ax.load(csv_name)
     print ('Load', csv_name, 'as', grid.shape)
-    solution = algorithm_x.solve_once(grid)
+    solution = ax.solve_once(grid)
 
 if __name__ == '__main__':
     #build_wiki_example()
