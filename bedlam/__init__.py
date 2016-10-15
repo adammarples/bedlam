60425888fa611d32985be57007b08b6a1d5cafa0
adammarples <adammarples@gmail.com>
2016-10-15 16:21:54 +0100

    import numpy

--- bedlam/__init__.py
+++ bedlam/__init__.py
@@ -2,9 +2,11 @@ GRID_DIR = 'grids'
 TEXT_DIR = 'text'
 SOLUTION_DIR = 'solutions'
 
+import numpy as np
+
 def load(filepath):
     return np.loadtxt(filepath, delimiter=",")
-    
+
 # import bedlam.algx
 # import bedlam.linked_lists
 # import bedlam.builder
