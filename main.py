87d50d48fd70e04e77511ec86befec25c6a0da74
adammarples <adammarples@gmail.com>
2016-10-15 11:47:21 +0100

    cProfile added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import algx
 import numpy as np
 from linked_lists import link_a_grid
+import cProfile
 
 def load(filepath):
     return np.loadtxt(filepath, delimiter=",")
@@ -11,11 +12,17 @@ def solve(name):
     root = link_a_grid(grid)
     algx.run_solver(name, root)
 
-if __name__ == '__main__':
+def main():
     """https://www.ocf.berkeley.edu/~jchu/publicportal/sudoku/0011047.pdf
     """
+    pass
     #solve('wiki_example')
-    #solve('soma')
+    solve('soma')
     #solve('example')
-    solve('bedlam')
+    #solve('bedlam')
     #solve('knuth')
+
+if __name__ == '__main__':
+    pass
+    #main()
+    cProfile.run("main()")
