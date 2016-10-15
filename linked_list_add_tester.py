fd7adf9a1d1b2d471391fd644a70ea65daaf5079
adammarples <adammarples@gmail.com>
2016-10-15 13:10:24 +0100

    succesful test of new joiner

---
+++ linked_list_add_tester.py
@@ -0,0 +1,51 @@
+import bedlam
+from bedlam.linked_lists import LinkedList, ColumnObject, Node
+
+def old_method():
+    root = ColumnObject(name='root')
+    headers = LinkedList()
+    headers.add_horizontally(root)
+    a = ColumnObject(name='a', size=1)
+    headers.add_horizontally(a)
+    b = ColumnObject(name='b', size=1)
+    headers.add_horizontally(b)
+    return root
+
+def self_join(node):
+    node.r = node
+    node.l = node
+
+def join_to(self, left):
+    self.l = left
+    left.r = self
+    while left.l.r is not self:
+        left = left.l
+    self.r = left
+    left.l = self
+
+def new_method():
+    Node.join_to = join_to
+    root = ColumnObject(name='root')
+    self_join(root)
+    a = ColumnObject(name='a', size=1)
+    self_join(a)
+    b = ColumnObject(name='b', size=1)
+    self_join(b)
+    a.join_to(root)
+    b.join_to(a)
+    return root
+
+
+
+
+def main():
+    old = old_method()
+    new = new_method()
+    for x in range(4):
+        print ((old.l.name, old.name, old.r.name), (new.l.name, new.name, new.r.name))
+        old = old.r
+        new = new.r
+
+
+if __name__ == '__main__':
+    main()
