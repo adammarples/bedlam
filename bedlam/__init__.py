3fdf422babcee3a97a09be854e23e20e589604ef
adammarples <adammarples@gmail.com>
2016-12-19 19:50:39 +0000

    remove comments

--- bedlam/__init__.py
+++ bedlam/__init__.py
@@ -9,10 +9,3 @@ import numpy as np
 
 def load(filepath):
     return np.loadtxt(filepath, delimiter=",")
-
-# import bedlam.algx
-# import bedlam.linked_lists
-# import bedlam.builder
-# import bedlam.build_matrix
-# import bedlam.coordinate_handler
-# import bedlam.shape_reader
