c0b68ab8ec64e385c0a8eba50504eb1895db56b5
adammarples <adammarples@gmail.com>
2016-12-19 22:06:36 +0000

    add facility to run on user defined txt files

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,8 @@
 import plac
-import bedlam.sudoku
+import os
+import shutil
+
+from bedlam import sudoku, SUDOKUS_DIR, SUDOKU_TEXT_DIR
 
 @plac.annotations(
     new = plac.Annotation('New Sudoku', 'option', 'n'),
@@ -11,9 +14,27 @@ def main(new, arc):
     a new one provided as a text file."""
 
     if new:
-        yield 'new={}'.format(new)
+        #yield 'new={}'.format(new)
+        name = save_as_sudoku(new)
+        sudoku.solve_and_build(name)
     if arc:
-        yield 'archived={}'.format(arc)
+        #yield 'archived={}'.format(arc)
+        if saved_checker(arc):
+            sudoku.solve_and_build(arc)
+        else:
+            yield (arc, 'is not a valid saved sudoku')
+
+def saved_checker(name):
+    """checks if the solution is a valid saved solution."""
+    path = os.path.join(SUDOKUS_DIR, '{}_box.txt'.format(name))
+    return format_checker(path)
+
+def save_as_sudoku(src):
+    path, filename = os.path.split(src)
+    name = os.path.splitext(filename)[0]
+    dst = os.path.join(SUDOKU_TEXT_DIR, filename)
+    shutil.copyfile(src, dst)
+    return name
 
 
 if __name__ == '__main__':
