02ca6a7cb66ec036276be4fce70b5abae6c129a9
adammarples <adammarples@gmail.com>
2017-02-02 19:36:50 +0000

    add saved_dict for running archives

--- bedlam/__init__.py
+++ bedlam/__init__.py
@@ -6,6 +6,12 @@ SUDOKUS_DIR = 'sudoku_boxes'
 SUDOKU_TEXT_DIR = 'sudoku_text'
 
 import numpy as np
-
 def load(filepath):
     return np.loadtxt(filepath, delimiter=",")
+
+saved_dict = {  'hoffmann': (6, 3),
+                'coffin': (6, 3),
+                'nob': (6, 3),
+                'soma': (7, 3),
+                'bedlam': (13, 4),
+            }
